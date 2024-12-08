736a76028f6cb8c2a538a4c2f54045a774d36458
Ceball0s <105499762+Ceball0s@users.noreply.github.com>
2024-12-08 18:04:28 -0500

    funcion dinamica

--- proyecto/punto1.py
+++ proyecto/punto1.py
@@ -21,8 +21,77 @@ class Terminal:
             "kill": self.costo_kill,
             "advance": self.costo_advance
         }
+
+    def terminal_dinamica(self, cadena1,cadena2):        
+        def terminal_dinamica_aux(i, j, memoria, pasos):
+            #verificar que la solucion ya se calculo
+            if memoria[i][j] != -1:
+                return memoria[i][j], pasos[i][j]
+
+            # Si llegamos al final de cadena2
+            if len(cadena2) == j:
+                if i < len(cadena1):
+                    memoria[i][j] = self.costo_kill
+                    pasos[i][j] = ["kill"]
+                    return self.costo_kill, ["kill"]
+                return 0, []
+
+            # Si llegamos al final de la terminal
+            if len(cadena1) == i:
+                memoria[i][j] = len(cadena2[j:]) * self.costo_insert
+                soluci = [f"insert {x}" for x in cadena2[j:]]
+                pasos[i][j] = soluci
+                return memoria[i][j], soluci
+
+            avanzar = float('inf')
+            paso_avance = []
+
+            # Si los caracteres son iguales, simplemente avanzamos
+            if cadena1[i] == cadena2[j]:
+                avanzar, paso_avance = terminal_dinamica_aux(i+1, j+1, memoria, pasos)
+                avanzar += self.costo_advance 
+            
+            # 1. Opción de insertar
+            costo_insertar, paso_insertar = terminal_dinamica_aux(i, j+1, memoria, pasos)
+            costo_insertar += self.costo_insert
+
+            # 2. Opción de eliminar
+            costo_eliminar, paso_eliminar = terminal_dinamica_aux(i+1, j, memoria, pasos)
+            costo_eliminar += self.costo_delete
+
+            # 3. Opción de reemplazar
+            costo_reemplazar, paso_reemplazar = terminal_dinamica_aux(i+1, j+1, memoria, pasos)
+            costo_reemplazar += self.costo_replace
+
+            # 4. Opción de matar (kill)
+            costo_kill_op, paso_kill_op = terminal_dinamica_aux(len(cadena1), j, memoria, pasos)
+            costo_kill_op += self.costo_kill
+
+            # Seleccionar la opción con el costo mínimo
+            min_costo, min_pasos = min(
+                (costo_insertar, ["insert " + cadena2[j]] + paso_insertar),
+                (costo_eliminar, ["delete " + cadena1[i]] + paso_eliminar),
+                (costo_reemplazar, ["replace " + cadena1[i] + " with " + cadena2[j]] + paso_reemplazar),
+                (costo_kill_op, ["kill"] + paso_kill_op),
+                (avanzar,["advance"] + paso_avance),
+                key=lambda x: x[0]
+            )
+
+            # Guardar la solución mínima
+            memoria[i][j] = min_costo
+            pasos[i][j] = min_pasos
+            return min_costo, min_pasos
+
+        n = len(cadena1)+1  # Número de columnas
+        m = len(cadena2)+1  # Número de filas
+        matriz = [[-1 for _ in range(m)] for _ in range(n)]
+        paso = [[-1 for _ in range(m)] for _ in range(n)]
+
+        return terminal_dinamica_aux(0,0,matriz,paso)
+
+
  
- def terminal_voraz(self, ca1, ca2):
+    def terminal_voraz(self, ca1, ca2):
         def terminal_voraz_aux(cadena1, cadena2, pasos, costo_total):
             if len(cadena1) == 0:
                 costo = self.costo_insert * len(cadena2)
